d7f583af0454403a2ce42a2647c75c878ee06fce
aafzal <ambreenfafzal@gmail.com>
2019-07-21 13:51:36 -0500

    Added bootstrap.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-var request = require ("request");
+// var request = require ("request");
 
 // // Our scraping tools
 // // Axios is a promised-based http library, similar to jQuery's Ajax method
